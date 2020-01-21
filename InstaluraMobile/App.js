2dd5a9b4e6bf76cdad04e23a0f63ce8d61b20a5e
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-01-21 10:16:58 -0300

    Exibindo várias fotos

--- InstaluraMobile/App.js
+++ InstaluraMobile/App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   View,
   Text,
@@ -16,16 +16,27 @@ import {
 
 const width = Dimensions.get('screen').width;
 
-const App: () => React$Node = () => {
+export default class App extends Component {
+  
+  render() {
+  const fotos = [
+    {id: 1, usuario: 'vinicius'},
+    {id: 2, usuario: 'leticia'},
+    {id: 3, usuario: 'joao'}
+  ];
+
   return (
+
     <View>
-      <Text>vinicius</Text>
-      <Image source={require('./resources/img/alura.jpg')}
-          style={{width: width, height: width}}/>
-    </View>
-  );
-};
+      {fotos.map(foto => 
+      <View key={foto.id}>
+        <Text>{foto.usuario}</Text>
+        <Image source={require('./resources/img/catalina.jpg')}
+            style={{width: width, height: width}}/>
+      </View>   
+        )}
+      </View>
+    );
+  }
+}
 
-
-
-export default App;
