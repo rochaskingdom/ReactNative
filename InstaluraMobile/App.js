dce9205b3a4af9e8b371beec6ae3aede5858ea4a
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-01-21 13:05:38 -0300

    Melhorando o layout

--- InstaluraMobile/App.js
+++ InstaluraMobile/App.js
@@ -12,31 +12,59 @@ import {
   Text,
   Image,
   Dimensions,
+  FlatList,
+  StyleSheet,
 } from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class App extends Component {
-  
   render() {
-  const fotos = [
-    {id: 1, usuario: 'vinicius'},
-    {id: 2, usuario: 'leticia'},
-    {id: 3, usuario: 'joao'}
-  ];
+
+    const fotos = [
+      {id: 1, usuario: 'vinicius'},
+      {id: 2, usuario: 'leticia'},
+      {id: 3, usuario: 'joao'}
+    ];
 
   return (
+    <FlatList
+      keyExtractor={item => item.id.toString()}
+      data={fotos}
+      renderItem={ ({item}) => 
+        <View>			
+			<View style={styles.cabecalho}>
+				<Image source={require('./resources/img/catalina.jpg')} 
+					style={styles.fotoDePerfil}/>
+            <Text>{item.usuario}</Text>
+			</View>
 
-    <View>
-      {fotos.map(foto => 
-      <View key={foto.id}>
-        <Text>{foto.usuario}</Text>
-        <Image source={require('./resources/img/catalina.jpg')}
-            style={{width: width, height: width}}/>
-      </View>   
-        )}
-      </View>
-    );
-  }
-}
+            <Image source={require('./resources/img/catalina.jpg')} 
+            	style={styles.foto}/>
+        </View>
+      }
+    />
+  );
+ }
+ }
 
+ const styles = StyleSheet.create({
+
+	 cabecalho: {
+		 margin: 10, 
+		 flexDirection: 'row', 
+		 alignItems: 'center'
+	},
+
+	fotoDePerfil: {
+		marginRight: 10, 
+		borderRadius: 20, 
+		width: 40, 
+		height: 40
+	},
+
+	foto: {
+		width:width, 
+		height:width
+	}
+ });
